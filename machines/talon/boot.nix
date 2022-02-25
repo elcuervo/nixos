13b8026b850955d92fe1844adba5cfe3b9af943a
elcuervo <elcuervo@elcuervo.net>
2022-02-24 23:14:52 -0300

    Define the device

--- machines/talon/boot.nix
+++ machines/talon/boot.nix
@@ -7,7 +7,7 @@
     initrd = {
       availableKernelModules = [ "xhci_pci" "thunderbolt" "nvme" "usb_storage" "sd_mod" ];
 
-      luks.devices.cryptdata.device = "/dev/disk/by-uuid/87842e2b-1ff9-4edc-aa67-513aaa005866";
+      luks.devices.cryptdata.device = "/dev/nvme0n1p2";
     };
 
     loader.grub = {
