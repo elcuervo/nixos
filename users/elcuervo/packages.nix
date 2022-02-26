052d8ff94448364103c677eb6dd98e747bd5e1e2
elcuervo <elcuervo>
2022-02-26 18:05:03 -0300

    Adds fonts and tools

--- users/elcuervo/packages.nix
+++ users/elcuervo/packages.nix
@@ -6,9 +6,26 @@
     home-manager.users."${config.my.username}" = { config, ... }: {
       home = {
         packages = with pkgs; [
+          (nerdfonts.override { fonts = [ "FiraCode" "DroidSansMono" "Hack" ]; })
+          input-fonts
+          font-awesome
+
           lazygit
+          ticker
+          graphviz
+          maim
+          s3cmd
+          xclip
+          heroku
+          nmap
+          ticker
+          gtop
+          calcurse
 
           # UI
+          vlc
+          bitwarden
+          _1password-gui
           zulip
           slack
           tdesktop
