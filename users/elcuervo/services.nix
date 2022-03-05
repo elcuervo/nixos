a45ad4bd2b51007adc21b5d73ffa6282941b0ecd
elcuervo <elcuervo@elcuervo.net>
2022-03-05 17:44:42 -0300

    Focus on the browser

--- users/elcuervo/services.nix
+++ users/elcuervo/services.nix
@@ -4,6 +4,20 @@
   config = with lib;
   {
     home-manager.users."${config.my.username}" = { config, ... }: {
+      dconf.settings = {
+        "org/gnome/settings-daemon/plugins/media-keys" = {
+          custom-keybindings = [
+            "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/"
+          ];
+        };
+
+        "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
+          binding = "<Alt>2";
+          command = "wmctrl -a Firefox";
+          name = "focus-browser";
+        };
+      };
+
       services = {
         gpg-agent = {
           enable = true;
