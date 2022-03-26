8d3afd9debbb48dbe62e2fed195959ad6de93464
elcuervo <elcuervo@elcuervo.net>
2022-03-26 18:56:11 -0300

    Sanity to the aliases

--- users/elcuervo/programs.nix
+++ users/elcuervo/programs.nix
@@ -32,10 +32,63 @@
             userName = "elcuervo";
             userEmail = "elcuervo@elcuervo.net";
 
+            delta.enable = true;
+
+            aliases = {
+              ci = "commit";
+              di = "diff";
+              st = "status";
+              co = "checkout";
+              br = "branch";
+              pu = "push";
+              pl = "pull";
+              cat = "cat-file -p";
+              rc = "rebase --continue";
+              rs = "rebase --skip";
+
+              unstage = "reset HEAD";
+              uncommit = "reset --soft HEAD^";
+
+              tags = "tag -l";
+              branches = "branch -a";
+              remotes = "remote -v";
+
+              amend = "commit --amend -C HEAD";
+            };
+
+            ignores = [
+              ".DS_Store"
+              ".direnv"
+            ];
+
             extraConfig = {
+              core = {
+                quotepath = false;
+              };
+
               init = {
                 defaultBranch = "main";
               };
+
+              github = {
+                user = "elcuervo";
+              };
+
+              pager = {
+                branch = false;
+              };
+
+              push = {
+                default = "simple";
+              };
+
+              format = {
+                signoff = true;
+              };
+            };
+
+            lfs = {
+              enable = true;
             };
           };
 
