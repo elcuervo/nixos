9510948a8827325aa30a266099366393202f8097
elcuervo <elcuervo@elcuervo.net>
2022-06-01 23:05:00 -0300

    Separate file

--- machines/modules/xserver.nix
+++ machines/modules/xserver.nix
@@ -25,33 +25,7 @@ in
 
           configuration = {
             default = {
-              text = ''
-                [ids]
-                *
-                [main]
-
-                # Maps capslock to escape when pressed and control when held.
-                capslock = overload(ctrl_vim, esc)
-
-                # ctrl_vim modifier layer; inherits from 'Ctrl' modifier layer
-
-                [ctrl_vim:C]
-
-                space = swap(vim_mode)
-
-                # vim_mode modifier layer; also inherits from 'Ctrl' modifier layer
-
-                [vim_mode:C]
-
-                h = left
-                j = down
-                k = up
-                l = right
-                # forward word
-                w = C-right
-                # backward word
-                b = C-left
-              '';
+              text = lib.strings.fileContents ./users/elcuervo/programs/keyd/default.conf;
             };
           };
         };
